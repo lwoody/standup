c9e901a5ce3056d3d715e98ca463c1179f9bd5d3
lwoody <lwoody@worksmobile.com>
2018-04-08 21:02:58 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">스탠드업 페이지</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          react로 스탠드업 페이지 개발
         </p>
       </div>
     );
